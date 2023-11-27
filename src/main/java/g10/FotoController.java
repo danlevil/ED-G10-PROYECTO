56a183fcefa031ccfeece2393a39243ab6c07f3b
Carlossv03 <carasala@espol.edu.ec>
2023-11-27 03:11:07 -0500

    Realizada las vistas para las fotos de los contactos

---
+++ src/main/java/g10/FotoController.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package g10;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Charlie
+ */
+public class FotoController implements Initializable {
+
+    @FXML
+    private ImageView imagen;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
---
+++ src/main/java/g10/FotosContactosController.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package g10;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author Charlie
+ */
+public class FotosContactosController implements Initializable {
+
+    @FXML
+    private ImageView regresar;
+    @FXML
+    private ImageView izquierda;
+    @FXML
+    private Button btSeleccionar;
+    @FXML
+    private ImageView derecha;
+    @FXML
+    private ImageView agregarFoto;
+    @FXML
+    private Label contacoId;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void regresarPantalla(MouseEvent event) {
+    }
+
+    @FXML
+    private void anterior(MouseEvent event) {
+    }
+
+    @FXML
+    private void seleccionarFoto(ActionEvent event) {
+    }
+
+    @FXML
+    private void siguiente(MouseEvent event) {
+    }
+
+    @FXML
+    private void agregarNuevaFoto(MouseEvent event) {
+    }
+    
+}
